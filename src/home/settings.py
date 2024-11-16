abc26c16a4538eaf0fbac9042fc8e53c8d26f8cb
Harshlimbachiya <limbachiyaharsh41@gmail.com>
2024-11-16 09:17:09 +0000

    staticfile prepared for dev env

--- src/home/settings.py
+++ src/home/settings.py
@@ -141,6 +141,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR= BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = BASE_DIR / "vendors"
+
+
+#SOURCE
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+#OUTPUT
+STATIC_ROOT=BASE_DIR / "local-cdn"
+
+if not DEBUG:
+    STATIC_ROOT=BASE_DIR.parent / "Prod-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
