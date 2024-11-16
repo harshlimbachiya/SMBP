3950745a8508dfec4d023884fbb971fc99b5bace
Harshlimbachiya <limbachiyaharsh41@gmail.com>
2024-11-16 10:54:04 +0000

    whitenoise added

--- src/home/settings.py
+++ src/home/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,27 +139,27 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "static/"
-STATICFILES_BASE_DIR= BASE_DIR / "staticfiles"
-STATICFILES_BASE_DIR.mkdir(exist_ok=True,parents=True)
-STATICFILES_VENDOR_DIR = BASE_DIR / "vendors"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
-
-#SOURCE
+# source(s) for python manage.py collectstatic 
 STATICFILES_DIRS = [
     STATICFILES_BASE_DIR
 ]
 
-#OUTPUT
-STATIC_ROOT=BASE_DIR / "local-cdn"
+# output for python manage.py collectstatic 
+# local cdn
+STATIC_ROOT = BASE_DIR / "local-cdn"
 
-if not DEBUG:
-    STATIC_ROOT=BASE_DIR.parent / "Prod-cdn"
+# < Django 4.2
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
