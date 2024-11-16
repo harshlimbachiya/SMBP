1e1997808711a6d0b97ffc0d7f351f89e687105d
harshlimbachiya <limbachiyaharsh41@gmail.com>
2024-11-16 10:09:42 +0530

    all file are added fir SMBP project

---
+++ src/home/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pathlib
+from visits.models import PageVisit
+
+
+this_dir=pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request,*args, **kwargs):
+    
+    qs= PageVisit.objects.all()
+    page_qs= PageVisit.objects.filter(path=request.path)
+    print(this_dir)
+    my_title= "home page"
+    my_context={  
+        "page_title": my_title,
+        "queryset" : page_qs.count(),
+        "percent": page_qs.count() * 100.0 / qs.count(),
+        "total_visit_count":qs.count(),
+    }
+    path= request.path
+    print("path",path)
+    html_=''
+    html_template='home.html'
+    PageVisit.objects.create(path=request.path)
+    return render(request,html_template,my_context)
